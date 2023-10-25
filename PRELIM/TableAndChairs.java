2a91b16738881c8d9ad94bcec659fd382ca80647
nanna-hue <148032860+nanna-hue@users.noreply.github.com>
2023-10-25 11:18:21 +0100

    Create TableAndChairs.java

---
+++ PRELIM/TableAndChairs.java
@@ -0,0 +1,19 @@
+public class TableAndChairs
+{
+   public static void main (String [] args)
+   {
+  
+      System.out.println("X                      X");
+      System.out.println("X                      X");
+      System.out.println("X     XXXXXXXXXXXX     X");
+      System.out.println("XXXXX X          X XXXXX");
+      System.out.println("XXXXX X          X XXXXX");
+      System.out.println("X   X X          X X   X");
+      
+   }
+   
+} 
+
+
+
+      

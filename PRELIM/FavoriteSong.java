b983728f53eb43b24400cd60df12a2c730054f14
nanna-hue <148032860+nanna-hue@users.noreply.github.com>
2023-10-25 11:19:06 +0100

    Create FavoriteSong.java

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,12 @@
+public class FavoriteSong
+{
+   public static void main (String [] args)
+   {
+
+   System.out.println("Bakit ka nag-iba?");
+   System.out.println("Mayro'n na bang iba?");
+   System.out.println("Sana sinabi mo");
+   System.out.println("Para 'di na umasang may tayo pa sa huli");
+   
+   }
+}

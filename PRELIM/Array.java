b43932b071ae7700d77f57b16e0fc567d74fca79
nanna-hue <148032860+nanna-hue@users.noreply.github.com>
2023-10-25 11:31:51 +0100

    Create Array.java

---
+++ PRELIM/Array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+  public class Array
+  {
+        public static void main(String [] args)
+          {
+             
+             String Email[] = {"nanna@gmail.com", "jkd@gmail.com"};
+             String Password[] = {"bantayot", "ilystv12345"};
+             
+             int i = 0;
+             
+             System.out.print("Email: " + Email[i]);
+             System.out.print("\nPassword: " + Password[i]);
+          }
+  }
